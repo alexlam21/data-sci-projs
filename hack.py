f432d186edb179869a024c9215175105006bf114
Bellabases <79431805+alexlam21@users.noreply.github.com>
2023-06-08 03:02:17 -0500

    Update hack.py
    finished hacker.py

--- hack.py
+++ hack.py
@@ -10,4 +10,26 @@ with open('compromised_users.txt', 'w') as compromised_user_file:
     compromised_user_file.write(compromised_user)
     compromised_user_file.write('\n')
 
-    import json
+import json
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {"recipient":"The Boss", "message":"Mission Succes"}
+  json.dump(boss_message_dict, boss_message)
+
+with open('new_passwords.csv', 'w') as new_passwords_obj:
+  slash_null_sig = """ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/"""
+  new_passwords_obj.write(slash_null_sig)
